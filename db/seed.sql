c051825e000b7a1c743b1df86605db33c635a0dc
Harpreet Aulakh <hkaulakh14@gmail.com>
2020-06-14 13:42:22 -0500

    routes,handlebars and added config connection

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('Monterey Melt Burger');
+INSERT INTO burgers (burger_name) VALUES ('Classic Burger');
