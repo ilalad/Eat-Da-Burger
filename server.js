d1447fe70a0d2228e08f861420a353c007d26996
Harpreet Aulakh <hkaulakh14@gmail.com>
2020-06-14 20:27:27 -0500

    created methods in orm.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var PORT = process.env.PORT || 8080;
-
+var orm = require("./config/orm.js");
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
