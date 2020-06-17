fd05f527493c7802c7cb9a3fe20bf79759c1947e
Harpreet Aulakh <hkaulakh14@gmail.com>
2020-06-16 19:40:49 -0500

    added .env file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,6 +17,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
+    console.log("body", req.body);
     burger.create([
         "burger_name", "devoured"
     ], [
